15ec66e6452bcedeb09d3b0d8a595f5e098b2685
pepe <xphssw@163.com>
2019-02-12 11:32:26 +0800

    [新增] 只出现一次的数字

---
+++ singleNumber.js
@@ -0,0 +1,13 @@
+/**
+ * https://leetcode-cn.com/submissions/detail/12596234/
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+  for (var i = 1; i < nums.length; i++) {
+      nums[0] = nums[0] ^ nums[i];
+  }
+  return nums[0];
+};
